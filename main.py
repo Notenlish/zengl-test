d6b410db6667ea1ea596fbf064094aa419cd77a9
Notenlish <71970100+Notenlish@users.noreply.github.com>
2024-06-29 19:51:50 +0300

    make shaders optional

--- main.py
+++ main.py
@@ -22,6 +22,9 @@ frag_shader = read_file("underwater3.frag")
 class App:
     def __init__(self) -> None:
         self.screen_size = (960, 540)
+        self.using_gpu = False
+        self.max_fps = 60
+
         self.pg_surf = self.init_screen()
         pygame.display.set_caption("Shaders in Browser Test")
         self.clock = pygame.time.Clock()
@@ -29,30 +32,37 @@ class App:
         self.time_elapsed = 0
         self.screenshot = pygame.image.load("screenshot.png").convert()
 
-        self.ctx = zengl.context()
+        if self.using_gpu:
+            self.ctx = zengl.context()
 
-        uniforms_map = {
-            "time": {
-                "value": lambda: struct.pack("f", self.time_elapsed),
-                "glsl_type": "float",
+            uniforms_map = {
+                "time": {
+                    "value": lambda: struct.pack("f", self.time_elapsed),
+                    "glsl_type": "float",
+                }
             }
-        }
 
-        self.screen_shader = ShaderPipeline(
-            self, uniforms_map, vert_shader, frag_shader
-        )
+            self.screen_shader = ShaderPipeline(
+                self, uniforms_map, vert_shader, frag_shader
+            )
 
     def init_screen(self):
-        display_kwargs = {
-            "size": self.screen_size,
-            "flags": pygame.OPENGL | pygame.DOUBLEBUF,
-            "vsync": True,
-        }
-        try:
-            return pygame.display.set_mode(**display_kwargs).convert_alpha()
-        except:
-            del display_kwargs["vsync"]
-            return pygame.display.set_mode(**display_kwargs).convert_alpha()
+        if self.using_gpu:
+            display_kwargs = {
+                "size": self.screen_size,
+                "flags": pygame.OPENGL | pygame.DOUBLEBUF,
+                "vsync": True,
+            }
+            try:
+                return pygame.display.set_mode(**display_kwargs).convert_alpha()
+            except:
+                del display_kwargs["vsync"]
+                return pygame.display.set_mode(**display_kwargs).convert_alpha()
+        else:
+            try:
+                return pygame.display.set_mode(size=self.screen_size, vsync=True)
+            except:
+                return pygame.display.set_mode(size=self.screen_size, vsync=False)
 
     def update(self):
         pass
@@ -61,10 +71,11 @@ class App:
         # cpu drawing
         self.pg_draw()
 
-        # zengl
-        self.ctx.new_frame()
-        self.screen_shader.render(self.pg_surf)
-        self.ctx.end_frame()
+        if self.using_gpu:
+            # zengl
+            self.ctx.new_frame()
+            self.screen_shader.render(self.pg_surf)
+            self.ctx.end_frame()
 
     def pg_draw(self):
         self.pg_surf.fill("black")
@@ -80,7 +91,7 @@ class App:
             self.update()
             self.render()
 
-            self.dt = self.clock.tick(60) / 1000
+            self.dt = self.clock.tick(self.max_fps) / 1000
             self.fps = self.clock.get_fps()
             self.time_elapsed += self.dt
 
