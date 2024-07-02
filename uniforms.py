b6d7ecd75d210b039d7efaa8e214eab23d1e341a
HmarVR <i.am.a.dum.idiot@outlook.com>
2024-07-02 14:58:29 +0300

    Update uniforms.py

--- uniforms.py
+++ uniforms.py
@@ -1,46 +1,24 @@
 import struct
+import math
 
-_palette = [
-    "FF5ba675",
-    "FF6bc96c",
-    "FFabdd64",
-    "FFfcef8d",
-    "FFffb879",
-    "FFea6262",
-    "FFcc425e",
-    "FFa32858",
-    "FF751756",
-    "FF390947",
-    "FF611851",
-    "FF873555",
-    "FFa6555f",
-    "FFc97373",
-    "FFf2ae99",
-    "FFffc3f2",
-    "FFee8fcb",
-    "FFd46eb3",
-    "FF873e84",
-    "FF1f102a",
-    "FF4a3052",
-    "FF7b5480",
-    "FFa6859f",
-    "FFd9bdc8",
-    "FFffffff",
-    "FFaee2ff",
-    "FF8db7ff",
-    "FF6d80fa",
-    "FF8465ec",
-    "FF834dc4",
-    "FF7d2da0",
-    "FF4e187c",
-]
+_palette = """000000
+21283f
+38526e
+3f86b0
+839dbf
+cee3ef
+"""
+
 
 # this gets run everytime theres a shader reload but you know what im too lazy
 palette = []
+_palette = _palette.splitlines()
+palette_size = len(_palette)
 for v in _palette:
     r, g, b = tuple(int(v[i : i + 2], 16) for i in (0, 2, 4))
     rgb = (r / 255, g / 255, b / 255)
     palette.append(rgb)
+
 
 def get_palette_buf():
     buffer = bytearray()
@@ -49,15 +27,22 @@ def get_palette_buf():
     return buffer
 
 
-
 def get_uniforms(self):
     uniforms_map = {
         "time": {
             "value": lambda: struct.pack("f", self.time_elapsed),
             "glsl_type": "float",
         },
+        "planetCenter": {
+            "value": lambda: struct.pack("ff", *self.planetPos),
+            "glsl_type": "vec2",
+        },
         "bodyRadius": {
             "value": lambda: struct.pack("f", self.bodyRadius),
+            "glsl_type": "float",
+        },
+        "cloudRadius": {
+            "value": lambda: struct.pack("f", self.cloudRadius),
             "glsl_type": "float",
         },
         "screenResolution": {
@@ -67,16 +52,21 @@ def get_uniforms(self):
             "glsl_type": "vec2",
         },
         "aspectRatio": {
+            "value": lambda: struct.pack("f", 16 / 9),
+            "glsl_type": "float",
+        },
+        "screenAspect": {
             "value": lambda: struct.pack("ff", 16, 9),
             "glsl_type": "vec2",
         },
         "lightDirection": {
-            "value": lambda: struct.pack("fff", 0.3, 0.3, -1),
+            "value": lambda: struct.pack("fff", math.cos(self.time_elapsed), 0.3, math.sin(self.time_elapsed)),
             "glsl_type": "vec3",
         },
-        "palette": {
-            "value": get_palette_buf,
-            "glsl_type": "vec3[32]"
-        }
+        "paletteSize": {
+            "value": lambda: struct.pack("i", palette_size),
+            "glsl_type": "int",
+        },
+        "palette": {"value": get_palette_buf, "glsl_type": f"vec3[{palette_size}]"},
     }
     return uniforms_map
